c12dce48bcc12eaf29abea687cec11337b7cfe36
Prabhakara Reddy Gaddam <prabhakargaddam1729@gmail.com>
2020-08-05 15:33:00 -0400

    Update keys_dev.js

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    googleClientID: '503898007040-gh5546emcid87gjhg47p5rhsgj61frdp.apps.googleusercontent.com',
-    googleClientSecret: '59nXaEuc-2NXBX52EPi9nW03'
+    googleClientID: GOOGLE_CLIENT_ID,
+    googleClientSecret: GOOGLE_SECRET
 }
